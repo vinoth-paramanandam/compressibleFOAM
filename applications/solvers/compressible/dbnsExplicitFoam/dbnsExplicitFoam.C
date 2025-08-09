2d6a6f77a005f971f95b53c8e00f946a9f023ce3
vinoth-paramanandam <viinoth.p@gmail.com>
2025-08-09 14:32:16 +0530

    Added folder structure
    Added Folder structure and the allwmake make files

---
+++ applications/solvers/compressible/dbnsExplicitFoam/dbnsExplicitFoam.C
@@ -0,0 +1,10 @@
+#include "fvCFD.H"
+
+int main(int argc, char *argv[])
+{
+    using namespace Foam;
+
+    Info << "\n Starting time loop \n" << endl;
+    Info << "\n End \n " << endl;
+    return 0;
+}
